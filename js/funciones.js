08cd52457e61fdbf61288acf31766cc6f768fca9
angtamrod <167316585+angtamrod@users.noreply.github.com>
2024-09-11 15:04:45 +0200

    actualizaciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,21 +1,24 @@
 // ESTE ARCHIVO JS SERVIRÁ PARA LEER LA TAREA Y PINTARLA EN EL ARCHIVO INDEX.HTML
-const contTareas= document.querySelector(".tareas");
-const formulario= document.querySelector("form");
-const inputTexto= document.querySelector(`form input[type="text"]`);
+//El archivo funciones.js es crucial para la funcionalidad de la aplicación, ya que maneja la carga, visualización y creación de tareas en la interfaz de usuario desde y hacia un backend. A continuación, te explico detalladamente cómo funciona cada parte de este archivo:
 
-//CARGA INICIAL DE LOS DATOS
-//Lo primero que hay que hacer es una llamada a los datos
-fetch("https://api-postgres-todo.onrender.com/tareas") //Aquí es donde he alojado los datos de mis tareas 
-.then(respuesta => respuesta.json())
+//1. SELECCIÓN DE LOS ELEMENTOS DEL DOM
+const contTareas= document.querySelector(".tareas");//Contenedor donde se muestran las tareas
+const formulario= document.querySelector("form");//Formulario para añadir nuevas tareas
+const inputTexto= document.querySelector(`form input[type="text"]`);//Campo de texto para introducir una nueva tarea
+
+//2. CARGA INICIAL DE LOS DATOS
+//Lo primero que hay que hacer es una llamada a los datos. Se realiza una llamada fetch a la API para obtener las tareas guardadas en el backend, a través de una URL 
+fetch("http://localhost:4000/tareas") //En esta URL es donde he alojado los datos de mis tareas. url anterior: https://api-postgres-todo.onrender.com/tareas
+.then(respuesta => respuesta.json())//La respuesta de la API es convertida a JSON.
 .then(tareas => {
-    //console.log(tareas);lo primero que hacemos es que la consola lea las tareas 
-
     //POSTGRES cuando edita pone esa tarea al final, para evitar esto haremos:
-    tareas.sort((a,b) => a.id - b.id).forEach(({id,tarea,terminada}) => {//en vez de hacer una función por cada tarea, vamos a desestructurar cada tarea, a través del id, tarea y terminada que son las cualidades disponibles de cada tarea
+    //en vez de hacer una función por cada tarea, vamos a desestructurar cada tarea, a través del id, tarea y terminada que son las cualidades disponibles de cada tarea
+    //Las tareas obtenidas son ordenadas por su id para asegurar un orden consistente:
+    tareas.sort((a,b) => a.id - b.id).forEach(({id,tarea,terminada}) => {
         new Tarea(id,tarea,terminada,contTareas); //De esta manera habiendo leído los datos del back, pintaremos una tarea en el front, con los datos de id, tarea y terminada (en este caso)
     });
-})//Hacer esto en un principio nos dará error porque al cambiar de dominio, porque no podemos hacer peticiones de un dominio a otro, el navegador le pedirá permisos que figurarán como no concedidos
-//PARA ELLO HAY QUE INSTALAR EL MÓDULO npm cors que sirve para conceder permisos desde nuestra API
+})//Hacer esto en un principio nos dará error porque al cambiar de dominio, porque no podemos hacer peticiones de un dominio a otro, el navegador le pedirá permisos que figurarán como no concedidos PARA ELLO HAY QUE INSTALAR EL MÓDULO npm cors en nuestro backend que sirve para conceder permisos desde nuestra API
+
 //Esto puede generar problemas de seguridad, pero a este nivel no es necesario, de todas maneras existe jwt.io donde se puede codificar unas credenciales para poder autenticar 
 
 
@@ -31,26 +34,35 @@ fetch("https://api-postgres-todo.onrender.com/tareas") //Aquí es donde he aloja
     }
 }); */
 
+//3. MANEJO DE LA CREACIÓN DE NUEVAS TAREAS
 
+//Se escucha el evento submit del formulario. Cuando envíamos un formulario
 formulario.addEventListener("submit", evento => {
+    //Cuando el formulario es enviado, se previene la acción por defecto (recargar la página).
     evento.preventDefault();
    
+    //Este if() verifica que el campo de texto no esté vacío y, de ser así, se crea una nueva tarea.
     if(inputTexto.value.trim() != ""){
         let tarea = inputTexto.value.trim();
 
-        fetch("https://api-postgres-todo.onrender.com/tareas/nueva", {//esto hará la petición a la base de datos 
+        //Se utiliza fetch con el método POST para enviar la nueva tarea a la API:
+        //esto hará la petición a la base de datos (Enviará el texto de la nueva tarea)
+        fetch("http://localhost:4000/tareas/nueva", { //url anterior: https://api-postgres-todo.onrender.com/tareas/nueva
             method : "POST", 
             body : JSON.stringify({tarea}),
             headers : {
-                "Content-type" : "application/json"
+                "Content-type" : "application/json" // Se especifica el tipo de contenido como JSON
             }
         })
-        .then(respuesta => respuesta.json())//Esto nos traerá la respuesta 
+        .then(respuesta => respuesta.json())//La respuesta se convierte a JSON y, si no hay errores, se instancia y muestra la nueva tarea en el frontend.
+        //De esa respuesta desestructuramos tanto el id como elerror 
         .then(({id,error}) => {
-            if(!error){//si no hay erro crearemos una tarea en la base de datos 
+            if(!error){
+                //si no hay error crearemos una tarea en la base de datos y la mostraremos en el front
                 new Tarea(id,tarea,false,contTareas);
-                return inputTexto.value = "";
-            }
+                return inputTexto.value = ""; //Esta línea limpia el campo del texto una vez hayamos creado una tarea
+
+            }// Muestra un mensaje en caso de error
             console.log("...mostrar error al usuario");
         });
     }
--- js/tarea.js
+++ js/tarea.js
@@ -1,6 +1,8 @@
-//TAREA.JS gestionará todo lo que son componentes 
+//TAREA.JS gestionará una clase tarea que representa cada tarea como un componente independiente dentro de la aplicación. Cada tarea se gestiona de manera autónoma mediante métodos que permiten crear, editar, actualizar el estado y borrar tareas. 
 
 //Gracias a la class cada componente es independiente son programas distintos, estás dando instruciones o recetas para crear componentes con unas características específicas las veces que sean
+
+//La clase Tarea construye un elemento div para cada tarea, que incluye texto, botones para editar, borrar y cambiar el estado.
 
 class Tarea{
     constructor(id,texto,estado,contenedor){//Esto lo que hará es añadir tareas al html (id, el texto, el estado de la tarea y el contenedor donde irá el contenido) No necesitaremos ni el estado ni el contenedor
@@ -8,7 +10,7 @@ class Tarea{
         this.texto = texto;
         this.editando = false;
         this.crearDOM(estado,contenedor);
-    }
+    }//El método crearDOM se encarga de ensamblar y añadir estos elementos al contenedor especificado.
     crearDOM(estado,contenedor){
         this.DOM = document.createElement("div");
         this.DOM.className = "tarea";
@@ -55,11 +57,21 @@ class Tarea{
 
         contenedor.appendChild(this.DOM);
     }
+    //2. EDICIÓN DE TAREAS:
+    //El método actualizarTexto permite editar el texto de una tarea. Si el usuario hace clic en el botón de editar, cambia el DOM para mostrar un campo de entrada (input) en lugar del texto. Es decir, cuando un usuario hace clic en el botón "editar", cambia el texto de la tarea a un campo editable (input). Una vez que el usuario edita el texto y hace clic en el botón "guardar", la función valida la entrada y, si es correcta, actualiza la tarea tanto en el frontend como en el backend.
     async actualizarTexto(){
+
+        //La función utiliza una propiedad this.editando que actúa como un interruptor (toggle) para determinar si la tarea está en modo edición o en modo visualización. Si this.editando es true, significa que la tarea está en modo edición, y el usuario está viendo un campo input para modificar el texto.
         if(this.editando){//Determinamos 2 acciones con una propiedad interna en este caso this.editando
+            //SI ESTAMOS EDITANDO:
+            //Aquí estamos extrayendo el texto temporal: Si el modo de edición está activo, la función toma el valor actual del campo input (el nuevo texto de la tarea) y lo guarda en textoTemporal.
             let textoTemporal = this.DOM.children[1].value.trim();//Extraemos el texto temporal del input
-            if(textoTemporal != "" && textoTemporal != this.texto){//Preguntamos si el usuario lo ha editado y si esa edición es correcta
-                let{error} = await fetch("https://api-postgres-todo.onrender.com/tareas/actualizar/1/" + this.id,{
+
+            
+            //Estamos preguntando si el usuario lo ha editado y si esa edición es correcta. Verificando que el textoTemporal no esté vacío(textoTemporal != "")  y que sea diferente (textoTemporal !=) al texto original (this.texto).
+            if(textoTemporal != "" && textoTemporal != this.texto){
+                //Si la validación es exitosa, envía una petición PUT al backend para actualizar el texto de la tarea en la base de datos:
+                let{error} = await fetch("http://localhost:4000/tareas/actualizar/1/" + this.id,{ //url anterior: https://api-postgres-todo.onrender.com/tareas/actualizar/1/
                     method : "PUT",
                     body : JSON.stringify({ tares : textoTemporal}),
                     headers : {
@@ -67,56 +79,74 @@ class Tarea{
                     }
                 }).then(respuesta => respuesta.json())
 
+                //Si la respuesta del backend no contiene errores (!error), se actualiza this.texto con el nuevo valor de textoTemporal.
                 if(!error){
                     this.texto = textoTemporal;
                 }else{
                     console.log("...error al usuario")
                 }
-                this.texto = textoTemporal; //Si esa condición se cumple hacemos el cambio (Aquí deberíamso hacer el fetch)
+                this.texto = textoTemporal; 
             }
+            // Actualiza el DOM para reflejar el texto actualizado y cambia la visibilidad
+            this.DOM.children[1].classList.remove("visible"); // Oculta el input quitando la clase "visible"
+            this.DOM.children[0].innerText = this.texto; // Muestra el texto actualizado
+            this.DOM.children[0].classList.add("visible"); // Muestra el texto como visible
+            this.DOM.children[2].innerText = "editar"; // Cambia el botón a "editar"
 
-            this.DOM.children[1].classList.remove("visible");
-            this.DOM.children[0].innerText = this.texto;
-            this.DOM.children[0].classList.add("visible");
-            this.DOM.children[2].innerText = "editar";
-
-        }else{//Si no estamos editando lo que hacemos es cambiar el DOM para que se permita editar 
-            this.DOM.children[0].classList.remove("visible");
-            this.DOM.children[1].value = this.texto;//Para coger el value de un input de formulario una vez el usuario ha modificado algo, tenemos que hacerlo directamente con el .value ya que la acción del usuario manda más que el JS (en la mayoría de los navegadores)
-            this.DOM.children[1].classList.add("visible");
-            this.DOM.children[2].innerText = "guardar";
+        //SI NO ESTAMOS EDITANDO(SIN modo edición):
+        }else{ 
+             // Cambia el DOM para permitir la edición
+            this.DOM.children[0].classList.remove("visible"); //Oculta el texto quitandole la clase visible
+            this.DOM.children[1].value = this.texto;//(Asigna el valor al input) Para coger el value de un input de formulario una vez el usuario ha modificado algo, tenemos que hacerlo directamente con el .value ya que la acción del usuario manda más que el JS (en la mayoría de los navegadores)
+            this.DOM.children[1].classList.add("visible");// Muestra el input añadiendole la clase visible
+            this.DOM.children[2].innerText = "guardar";// Cambia el texto del botón a "guardar"
         }
-        this.editando = !this.editando; //ES UN TOGGLE si estaba true se tendría que poner false y si no no
-    }
+        this.editando = !this.editando; //ES UN TOGGLE que invierte el estado de edición, si estaba true se tendría que poner false y viceversa
+
+
+    }//El método actualizarEstado() cambia visualmente el estado de la tarea en la interfaz de usuario y envía una petición PUT al backend para actualizar el estado en la base de datos. 
     actualizarEstado(){
+        //La función devuelve una nueva promesa. Esto permite a otros métodos o funciones manejar el resultado de la actualización, ejecutando acciones adicionales si la actualización es exitosa o fallida.
        return new Promise((ok,ko) => {
         // ok();
-         fetch("https://api-postgres-todo.onrender.com/tareas/actualizar/2/" + this.id, {
+        //Como la petición no envía un cuerpo JSON ya que solo necesita cambiar el estado (posiblemente un toggle entre completada y no completada).
+         fetch("http://localhost:4000/tareas/actualizar/2/" + this.id, { //URL ANTERIOR https://api-postgres-todo.onrender.com/tareas/actualizar/2/
             method : "PUT"
          })
-         .then(respuesta => respuesta.json())
-         .then(({error}) => {
-            !error ? ok() : ko(); //Si no hay error invocaremos ok() si no invocaremos ko()
+         .then(respuesta => respuesta.json()) //Esto convierte la respuesta a JSON
+         .then(({error}) => {// Una vez que el servidor responde, se evalúa si la actualización fue exitosa o no utilizando la propiedad error de la respuesta JSON.
+
+            //Si no hay error (!error), la promesa se resuelve invocando ok(), lo que indica que la actualización fue exitosa. Si hay un error, la promesa se rechaza invocando ko()
+            !error ? ok() : ko(); 
+
          });
        });
-    }
-    borrarTarea(){
+    }borrarTarea(){ 
         // this.DOM.remove(); Hasta este punto cuando borrabamos la tarea solo se borraba momentáneamente en el front, pero no en el back.
-
-        fetch("https://api-postgres-todo.onrender.com/tareas/borrar/" + this.id, {//Esto la borra SOLO del back
+        //La función utiliza fetch para enviar una petición DELETE al endpoint del backend https://api-postgres-todo.onrender.com/tareas/borrar/ seguido del ID de la tarea.
+        fetch("http://localhost:4000/tareas/borrar/" + this.id, {//Esto la borra SOLO del back. Este endpoint está configurado para manejar la eliminación de la tarea en la base de datos. //URL ANTERIOR https://api-postgres-todo.onrender.com/tareas/borrar/
             method : "DELETE"
         })
-        .then(respuesta => respuesta.json())
+        .then(respuesta => respuesta.json())//La respuesta del servidor se convierte a formato JSON utilizando .then(respuesta => respuesta.json()).
         .then(({resultado,error}) => {
+            //La respuesta JSON del servidor se desestructura para obtener resultado y error.
+            //Se verifica si no hay un error (!error). 
             if(!error){
+                //Si no hay error, se procede a eliminar la tarea del frontend invocando this.DOM.remove().
                 return this.DOM.remove();
-            }
+            }//En caso de que haya un error, se imprime un mensaje en la consola (console.log("...mostrar error al usarlo");) para indicar que hubo un problema al intentar borrar la tarea.
             console.log("...mostrar error al usarlo");
         });
        
     }
 }
 
+
+/*La función borrarTarea() en la clase Tarea se encarga de eliminar una tarea tanto del frontend (la interfaz de usuario) como del backend (la base de datos):
+        - Elimina visualmente la tarea del DOM en el frontend.
+        - Envía una petición DELETE al backend para eliminar la tarea de la base de datos.
+
+*/
 
 
 // postgresql://bbdd_tareas_sy1q_user:L0mXclFk4O742IU72rGBRD0al1Z2HJoV@dpg-cqf9ec0gph6c73ba7ij0-a.frankfurt-postgres.render.com/bbdd_tareas_sy1q
